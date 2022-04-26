ead4175e800f8ff36bba83a44718165a18be9909
qihang.liu <qihang.liu@tuya.com>
2022-04-26 09:59:10 +0800

    update

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ import os
 from openpyxl.utils import get_column_letter
 
 
+def _get_excel_total_row(path: str):
+    wb = openpyxl.load_workbook(path)
+    sheet_names = wb.sheetnames
+    sheet_names.sort()
+    total_row = 0
+    for sheet_name in sheet_names:
+        sheet = wb[sheet_name]
+        row = sheet.max_row
+        total_row += row - 1
+    return total_row
+
+
 def _copy_xlsx(path: str, save_path: str, copy_image=True):
     wb = openpyxl.load_workbook(path)
     wb2 = openpyxl.Workbook()
@@ -60,46 +72,56 @@ def _copy_xlsx(path: str, save_path: str, copy_image=True):
     print('Done.')
 
 
-def check_file_extend(file_name, file_ext):
-    if not str(file_name).endswith(file_ext):
-        raise ValueError("invalid file extend, file is " + file_name + ", ext is " + file_ext)
+def check_file_ext_filterend(file_name, file_ext_filter):
+    if not str(file_name).endswith(file_ext_filter):
+        raise ValueError("invalid file extend, file is " + file_name + ", ext is " + file_ext_filter)
 
 
-def copy_file(src, target, file_ext='xlsx', copy_image=True):
-    check_file_extend(src, file_ext)
-    check_file_extend(target, file_ext)
+def copy_file(src, target, file_ext_filter='xlsx', copy_image=True):
+    check_file_ext_filterend(src, file_ext_filter)
+    check_file_ext_filterend(target, file_ext_filter)
     _copy_xlsx(src, target, copy_image)
 
 
-def copy_dir(src, target, file_ext='xlsx', copy_image=True):
+def copy_dir(src, target, file_ext_filter='xlsx', copy_image=True):
     if not os.path.isdir(target):
         print("create directory " + target)
         os.mkdir(target)
 
     files = os.listdir(src)
     for file in files:
-        if os.path.isdir(file) or not str(file).endswith(file_ext):
+        if os.path.isdir(file) or not str(file).endswith(file_ext_filter):
             continue
         src_file = src + os.sep + file
         dst_file = target + os.sep + file
-        copy_file(src_file, dst_file, file_ext, copy_image)
+        copy_file(src_file, dst_file, file_ext_filter, copy_image)
 
 
-def test(mode, src, dst):
-    copy_handler = copy_dir if 'dir' == mode else copy_file
-    copy_handler(src, dst)
+def get_excel_row_in_dir(dir_path: str, file_ext_filter='xlsx'):
+    if not os.path.isdir(dir_path):
+        print("create directory " + target)
+        os.mkdir(target)
+
+    files = os.listdir(dir_path)
+    for file in files:
+        if os.path.isdir(file) or not str(file).endswith(file_ext_filter):
+            continue
+        src_file = dir_path + os.sep + file
+        total_line = _get_excel_total_row(src_file)
+        print(file + ', 共有: ' + str(total_line))
 
 
 @click.command()
 @click.option('--mode', '-m', help='mode', default='dir')
 @click.option('--src', "-s", help='source file', required=True)
 @click.option('--target', "-t", help='target file', required=True)
-@click.option('--file_ext', "-e", help='file extend', default='xlsx')
+@click.option('--file_ext_filter', "-e", help='file extend', default='xlsx')
 @click.option('--copy_image', '-i', help='copy image', default=True)
-def main(mode, src, dst, file_ext, copy_image):
+def main(mode, src, dst, file_ext_filter, copy_image):
     copy_handler = copy_dir if 'dir' == mode else copy_file
-    copy_handler(src, dst, file_ext, copy_image)
+    copy_handler(src, dst, file_ext_filter, copy_image)
 
 
 if __name__ == "__main__":
-    main()
+    #copy_dir('/Users/kun/Desktop/市民卡业务带图', '/Users/kun/Desktop/市民卡业务带图1')
+    get_excel_row_in_dir('/Users/kun/Desktop/市民卡业务带图')
